228c7cffa10ee1b28aa8e92517283aa019ad5de9
teejay74 <teejay744@gmail.com>
2021-02-28 13:02:33 +0500

    change ws-server

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import AppController from './AppController';
 
-const manager = new AppController('ws://localhost:7171');
-// const manager = new AppController('wss://still-mountain-04235.herokuapp.com');
+// const manager = new AppController('ws://localhost:7171');
+const manager = new AppController('wss://still-mountain-04235.herokuapp.com');
 manager.init();
